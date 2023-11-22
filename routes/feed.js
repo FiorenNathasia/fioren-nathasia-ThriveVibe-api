f787a77c02035f5f25aebbbd2cbc4dc02ae2de14
FiorenNathasia <fiorennathasia@gmail.com>
2023-11-22 17:33:21 +0000

    Add to getFeed endpoint.

--- routes/feed.js
+++ routes/feed.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const feedController = require("../controller/feedController");
 
-router.get("/", feedController.getFeed);
+router.get("/feedlist", feedController.getFeed);
 
 module.exports = router;
